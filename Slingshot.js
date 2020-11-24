c513c218d77935203137e97deeb303b084e3769a
Saanvi Sharma <72926177+saanvisharm@users.noreply.github.com>
2020-11-24 19:39:41 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,23 @@
+class slingshot{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.06,
+            length:15
+        }
+        this.pointB=pointB;
+        this.sling=Constraint.create(options);
+        World.add(world,this.sling);
+    }
+    fly(){
+        this.sling.bodyA=null;
+    }
+    display(){
+        if(this.sling.bodyA){
+    var pointA=this.sling.bodyA.position;
+    var pointB=this.pointB;
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+  }
+}
+}
